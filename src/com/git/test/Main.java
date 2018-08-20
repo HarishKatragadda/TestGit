02094f11fc271acdf565545225badcb8e55a330b
Himanshusinghcs0036 <Himanshusinghcs0036@gmail.com>
2018-08-20 18:15:24 +0530

    Update Main.java

--- src/com/git/test/Main.java
+++ src/com/git/test/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("This is to test GIT");
+            System.out.println("This is First File");
+	    System.out.println("This is to test GIT");
     }
 }
